efaea5fd3cf79c5719da4c19dd69c08e1cf57e70
antor sarker <antorsarker55@gmail.com>
2026-03-18 16:42:01 +0600

    feat: add category filter and search functionality to services API

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -2,7 +2,21 @@ const Services = require("../models/service.js");
 
 exports.getServices = async (req, res) => {
   try {
-    const services = await Services.find();
+    const { search, category } = req.query;
+    const query = {};
+
+    //filter by category
+    if (category && category !== "all") query.category = category;
+    //search services
+    else if (search) {
+      query.$or = [
+        { category: { $regex: search, $options: "i" } },
+        { description: { $regex: search, $options: "i" } },
+        { title: { $regex: search, $options: "i" } },
+      ];
+    }
+
+    const services = await Services.find(query);
     res.status(200).json(services);
   } catch (error) {
     res.status(500).json({ error: "services data not found!" });
